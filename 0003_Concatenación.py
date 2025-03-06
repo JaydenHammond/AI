a22b02f0c81df9472e3787561039ec31a3c34e29
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-05 23:35:03 -0600

    Add files via upload

---
+++ 0003_Concatenación.py
@@ -0,0 +1,18 @@
+concatena = "Hotel " + "Trivago"
+print(concatena)
+
+p1 = "Inteligencia "
+p2 = "Artificial"
+nombre = p1 + p2
+print(nombre)
+
+palabra1 = "Come"
+print(palabra1 + " bien")
+
+nom = "Jayden"
+apellido1 = "Hammond"
+apellido2 = "Caballero"
+
+nom_completo = nom + " " + apellido1 + " " + apellido2
+
+print(nom_completo)
