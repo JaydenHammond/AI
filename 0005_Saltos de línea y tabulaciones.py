09ce71a3416a7992f91d4bb6dffeaca6ce5a4829
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-07 05:41:48 -0600

    Saltos de línea y tabulaciones en Python
    En este capítulo, veremos como hacer saltos de línea y tabulaciones en Python.

---
+++ 0005_Saltos de línea y tabulaciones.py
@@ -0,0 +1,6 @@
+#scribe el contenido (strings) de todos estos print() en uno solo con un string.
+#Debes utilizar los saltos de línea para que quede cada  lenguaje de
+#programación escrito en una línea. Al final, añade dos lenguajes más.
+#Además, quiero que añadas un guion al principio de cada fila y un punto
+#al final. Debería quedarte algo así:
+print("Nombres de amigos\n\t-Jayden.\n\t-Jose.\n\t-Alejandro.\n\t-Omar.\n\t-Neri.\n\t-July.")
