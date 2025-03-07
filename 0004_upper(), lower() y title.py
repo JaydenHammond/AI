526b1712465a2396296a405cf12ba1560f678525
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-07 05:30:08 -0600

    Upper(), lower() y title
    Dar formato a los textos de los strings. Lo haremos con los métodos upper(), lower() y title() de Python.

---
+++ 0004_upper(), lower() y title.py
@@ -0,0 +1,10 @@
+#Los métodos upper(), lower() y title de Python
+#Pon en mayúsculas la primera letra de cada palabra de este nombre utilizando uno de los métodos del capítulo:
+nom = "jayden hammond caballero".title()
+print(nom)
+#Deja esta frase totalmente en letras minúsculas utilizando uno de los métodos del capítulo:
+frase = "Esta Es Una Frase Para Ser Formateada".lower()
+print(frase)
+#Deja esta frase totalmente en letras mayúsculas:
+may = "Esta Es Una Frase Para Ser Formateada".upper()
+print(may)
