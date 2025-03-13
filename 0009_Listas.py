2ae4d70be2de3fc37b0878c157c4dcb775db6e18
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-12 23:38:40 -0600

    0009_Listas
    Una lista, explicado de forma sencilla, es una variable con múltiples valores.

---
+++ 0009_Listas.py
@@ -0,0 +1,11 @@
+#De la siguiente lista, ¿qué color está en la posición 3?
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[3])
+
+#¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+print(colores.index("rojo"))
+print(colores.index("rosa"))
+
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+numeros = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(numeros)
