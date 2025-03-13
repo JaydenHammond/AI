5df3d7cfce2bd144bc41a0f4e40efa01f8041449
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-13 00:13:21 -0600

    0015_Insertar elementos con insert()
    Como añadir elementos en las listas en la posición que quieras con el método insert() de Python.

---
+++ 0015_Insertar elementos con insert().py
@@ -0,0 +1,8 @@
+#Añade a la siguiente lista los colores 'magenta' y 'turquesa' utilizando
+#insert(). Tendrás que posicionar 'magenta' en la posición siguiente
+#a la de 'lila' y 'turquesa' en la penúltima posición.
+#Deberás hacer esto utilizando posiciones de lista negativas.
+colores =['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.insert(6, 'magenta')
+colores.insert(-1, 'turquesa')#Miku#
+print("Nuevos colores:",colores)
