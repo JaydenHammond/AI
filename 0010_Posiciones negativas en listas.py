8694093b2598b8391b792aad5499071a619e77d3
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-12 23:45:43 -0600

    0010_Posiciones negativas en listas
    Las posiciones negativas, las cuales empiezan a contar a la inversa, desde el último elemento al primero.

---
+++ 0010_Posiciones negativas en listas.py
@@ -0,0 +1,18 @@
+#Utiliza las posiciones negativas para acceder e imprimir algunos de
+#los colores de esta lista. Los colores a los que tienes que acceder
+#son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# naranja
+print(colores[-1])
+
+# amarillo
+print(colores[-7])
+
+# lila
+print(colores[-5])
+
+# blanco
+print(colores[-2])
+
+# rojo
+print(colores[-10])
