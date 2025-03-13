4e6708d5e4ed2e7ea4040bdeddb0fd132a1b05ed
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-12 23:52:00 -0600

    0011_Eliminación de elementos con del
    Para eliminar de esta lista un elemento lo puedes hacer con del y el número de posición en la lista.

---
+++ 0011_Eliminación de elementos con del.py
@@ -0,0 +1,9 @@
+#De esta lista, elimina los colores 'azul', 'marrón', 'negro' y 'rosa'.
+#Debes utilizar al menos una vez las posiciones negativas para eliminar
+#un color. Después, imprime la lista para ver los colores que se han eliminado.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del colores[1] #azul
+del colores[3] #marron
+del colores[4] #negro
+del colores[4] #rosa
+print("Los colores que hay son:",colores)
