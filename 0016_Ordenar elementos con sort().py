0d24450e682d6369eada84454af50fae6974a904
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-14 20:01:50 -0600

    0016_Ordenar elementos con sort()
    ordenar elementos en tus listas en orden alfabético con el método sort() y a utilizar también sorted().

---
+++ 0016_Ordenar elementos con sort().py
@@ -0,0 +1,6 @@
+#Ordena la siguiente lista en orden alfabético descendente
+#(de la letra 'z' a la 'a').
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort(reverse=True)
+print("Colores ordenados e a-z:",colores)
