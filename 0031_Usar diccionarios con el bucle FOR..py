77cde6ee64f0de4979889862ffa73b7f74748e76
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-18 02:44:54 -0600

    Usar diccionarios con el bucle FOR
    Empecemos este capítulo viendo como modificar valores de un diccionario. En el siguiente ejemplo, le doy un valor diferente al 'Precio'.

---
+++ 0031_Usar diccionarios con el bucle FOR..py
@@ -0,0 +1,10 @@
+##Itera el diccionario teclado1 con un solo bucle for que muestre esto en la consola:
+##Resultado:
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+for x in teclado1:
+	print(x, '=', teclado1[x] + '.')
