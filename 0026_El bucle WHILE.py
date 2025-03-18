0cc7ff97d99da3db2af38932cd8b2f7f67d9d5e6
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-18 02:24:55 -0600

    0026_El bucle WHILE
    Con el bucle while podrás ejecutar una serie de declaraciones siempre que la condición se cumpla, que sea verdadera. Una vez se convierta en falsa, va a dejar de ejecutar el código del bucle.

---
+++ 0026_El bucle WHILE.py
@@ -0,0 +1,19 @@
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5
+x = 0
+while x <= 15:
+    print(x)
+    x += 5
+
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+x = 0
+while x >= -100:
+    print(x)
+    x -= 20
+
+#Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de
+#1 y que muestre en cada ejecución esta frase con el valor de ejecución
+#correspondiente: 'El valor del bucle es 10'
+x = 10
+while x >= 0:
+    print('El valor de x es: ', x)
+    x -= 1
