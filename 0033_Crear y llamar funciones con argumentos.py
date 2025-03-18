8a05d2c2f3132a1b46959d5b20236f30600cb6f8
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-18 02:51:11 -0600

    0033_Crear y llamar funciones con argumentos.
    Empezamos en el curso con las funciones de Python, las cuales son bloques de código que solo se van a ejecutar si se les llama.

---
+++ 0033_Crear y llamar funciones con argumentos.py
@@ -0,0 +1,12 @@
+##Crea una función que realice una suma. Para ello, tendrás que añadir
+##dos argumentos(numero1 y numero2). En su interior, especificarás un print()
+##que muestre el resultado de la suma. Deberás hacer tres llamadas que como
+##resultado de la suma den los valores 30, 50 y 57000. Los números que
+##introduzcas en la llamada pueden ser los que quieras siempre que coincidan
+##los resultados en el print()
+def suma(no1, no2):
+	print(no1 + no2)
+
+suma(1, 29)
+suma(15, 35)
+suma(50000, 7000)
