342d4b49770cf88aa81720817d391176deadba4b
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2025-03-18 02:41:44 -0600

    0030_El método dict()
    Para imprimir un diccionario entero, puedes hacerlo pasándole el nombre a un print()

---
+++ 0030_El método dict().py
@@ -0,0 +1,15 @@
+##Del diccionario teclado2 del capítulo, muestra los elementos Modelo
+##y Precio con presentación en un print(). El resultado será esto en la consola:
+mouse1 = {
+	'Categoría': 'Mouse',
+	'Modelo': 'Logitech G305',
+	'Precio': '29.99'
+}
+
+mouse2 = {
+	'Categoría': 'Mouse',
+	'Modelo': 'G502 Lighthouse',
+	'Precio': '89.99'
+}
+
+print('El modelo', mouse1['Modelo'], 'cuesta', mouse2['Precio'], '$.')
